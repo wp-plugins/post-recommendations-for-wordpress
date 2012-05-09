f001e1a8dcf9c7c7ef38566f98f8430c5d2d68a1
ankurt <ankurt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-09 14:38:59 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/post-recommendations-for-wordpress/tags/1.1@541845 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ javascripts/recommendationsApp.js
@@ -0,0 +1,171 @@
+(function($) {	
+	// cache
+	var wrapper = $('.wp-recommendations ul'); 
+	var posts = $('.wp-recommendations ul li'); 
+	
+	var form = $('#wp-recommendations-form'); 
+	var select = $('#wp-recommendations-form select'); 
+	
+	var load = $('img.load'); 
+	
+	var recommendationsApp = { 
+		fixCaptions: function() { 
+			console.log("called"); 
+			var titles = $('.wp-recommendations-meta h6 a'); 
+			console.log(titles); 
+			titles.addClass('wp-recommendations-meta-surpress'); 
+			
+			titles.each ( function() { 
+				var title_len = parseInt($(this).text().length);
+				if ( title_len > 27 ) { 
+					$(this).attr('id', $(this).text() ); 
+					var new_text = $(this).text().substr( 0, 27 ); 
+					$(this).text(new_text + "..."); 
+				} 	
+			}); 
+		},
+		
+		clear: function() { 
+				wrapper.empty(); 	 
+		}, 		
+		
+		make: function( post ) { 
+	
+			var newElement = $("<li></li>").prependTo(wrapper); 
+			var src = encodeURI ( tmth.p + "?src=" + post.imgsrc + "&w=" + image.size[0] + "&h=" + image.size[1] ); 	
+			
+			var title_len = parseInt(post.title.length);
+			 
+			if ( title_len > 27 ) { 
+				var title_id = post.title; 
+				var new_text = post.title.substr( 0, 27 ); 
+				var elipse = new_text + "...";
+				newElement.append(
+					"<div class='wp-recommendations-meta'><h6><a class='wp-recommendations-meta-surpress' id='"+post.title+"' href='"+post.link+"'>"+elipse+"</a></h6></div>"
+				); 
+				
+			} else { 
+				newElement.append(
+					"<div class='wp-recommendations-meta'><h6><a class='' href='"+post.link+"'>"+post.title+"</a></h6></div>"
+				); 
+			} 	
+	
+			var img = $("<img class='thumbnail' />").attr( 'src', src )
+														.load( function() { 
+															newElement.prepend(img); 			
+														}); 	
+		},
+		
+		freeze: function() { 
+			wrapper.height(wrapper.height());
+		},  
+	
+	}; //recommendationsApp
+	
+	recommendationsApp.fixCaptions(); 
+	
+	$(".wp-recommendations ul li").live({ 
+			mouseenter: 
+				function() { 
+					var text_node = $(this).find('h6 a'); 
+					var excerpt = text_node.text();
+					
+					text_node.removeClass('wp-recommendations-meta-surpress');
+					 
+					if ( text_node.attr('id') != undefined ) { 
+						text_node.text( text_node.attr('id') ); 
+						text_node.attr('id', excerpt );  
+					}  
+				}, 
+			mouseleave: 
+				function() { 
+					$(this).find('h6 a').addClass('wp-recommendations-meta-surpress'); 
+					
+					var text_node = $(this).find('h6 a')
+					var full_text = text_node.text(); 
+					
+					if ( text_node.attr('id') != undefined ) { 
+						text_node.text( text_node.attr('id') ); 
+						text_node.attr('id', full_text );  
+					}  
+				}
+		} 
+	);
+	
+	select.change( function() { 
+		form.submit(); 
+	});  
+	
+	form.submit(
+		function(event) { 
+			event.preventDefault(); 
+			
+			load.show(); 
+			
+			// discover what's being requested
+			var selected = select.val(); 
+			var selObj = select.find("option[value='"+selected+"']");
+			
+			// the data object
+			var data = { 
+				'action': 'get_posts', 
+				'type': selObj.attr('value'), 
+				'id': selObj.attr('id'),
+				'num': image.num, 
+				'orderby': image.orderby
+			}; 
+						
+			// freeze the ul 
+			recommendationsApp.freeze();
+			
+			// call the clear method to empty the ul 
+			recommendationsApp.clear(); 
+			
+			// the post request
+			$.post(
+				ajaxurl,
+				data, 
+				function (response) { 
+					$.each( response, function( index, post ) { 
+						recommendationsApp.make( post );
+					}); 
+					load.hide();  
+				},
+				'json'
+			);			
+		}
+	); // form.submit()
+	
+	posts.click( function() { 
+		var src = $(this).find("a").attr('href');
+		window.location = src; 
+	}); 
+
+})( jQuery ); 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
---
+++ wp_recommendations.php
@@ -0,0 +1,193 @@
+<?php
+/*
+Plugin Name: Post Recommendations for WordPress
+Plugin URI: http://oldmill1.github.com/post-recommendations-for-wordpress/
+Description: Give your visitors more posts to see. 
+Version: 1.1
+Author: Ankur Taxali
+Author URI: http://oldmill1.github.com/
+License: GPL2
+*/
+
+/* 
+		This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/ 
+
+include 'recommendationsApp.php'; 
+
+wp_enqueue_style(
+	'wp-recommendations-style', 
+	plugins_url( 'stylesheets/stylesheet.css', __FILE__ ) 
+); 
+
+wp_enqueue_script("jquery"); // load jQuery for those who don't have it
+
+wp_enqueue_script(
+	'recommendationsAppJavascript', 
+	plugins_url( 'javascripts/recommendationsApp.js', __FILE__ ), 
+	NULL, 
+	NULL, 
+	TRUE 
+); 
+
+wp_enqueue_script(
+	'jqueryui', 
+	plugins_url( 'javascripts/jquery-ui.min.js', __FILE__ ), 
+	NULL, 
+	NULL, 
+	TRUE 
+); 
+
+
+global $app; 
+
+$app = new recommendationsApp(); 
+
+// this can only be done after an app has been started... 
+
+$tmth_data = array( 'p' => $app->get_timthumb_path() ); 
+wp_localize_script( 'recommendationsAppJavascript', 'tmth', $tmth_data );
+
+
+// template tag 
+function wp_recommendations( $args ) { 
+	global $post; 
+	global $app; 
+
+	$defaults = array( 
+		'postID' => $post->ID,
+		'size' => null,  
+		'numberposts' => 6, 
+		'orderby' => 'rand', 
+		'show' => true
+	);  
+		
+	$args = wp_parse_args( $args, $defaults ); 
+	
+	$app->build_recommendations( $args );  
+	 
+} 
+
+// shortcode 
+function recommend_something_func( $atts ) { 
+	global $post; 
+	
+	extract( shortcode_atts( array(
+			'post_id' => $post->ID,
+			'size' => null,  
+			'numberposts' => 6, 
+			'orderby' => 'rand', 
+			'show' => false
+		), $atts ) );
+		
+	// if size is set, convert it to an array 
+	if ( ! is_null($size) ) { 
+		$size = explode(",", $size, 2 );
+		$size[0] = intval($size[0]); 
+		$size[1] = intval($size[1]); 
+	} 
+		
+	$args = array( 
+		'postID' => $post_id,
+		'size' => $size,  
+		'numberposts' => $numberposts, 
+		'orderby' => $orderby, 
+		'show' => false
+	); 
+	
+	$app = new recommendationsApp(); 
+	return $app->build_recommendations( $args );  
+}
+
+
+add_shortcode( 'recommend_posts_for_me', 'recommend_something_func' );
+
+// define ajaxurl on the front end 
+add_action('wp_head','wprecommendations_ajaxurl');
+
+function wprecommendations_ajaxurl() {
+?>
+<script type="text/javascript">
+var ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>';
+</script>
+<?php
+}
+
+add_action('wp_ajax_my_action', 'prefix_ajax_get_posts' ); 
+
+add_action('wp_ajax_nopriv_get_posts', 'prefix_ajax_get_posts');
+
+
+// the main ajax request function
+function prefix_ajax_get_posts() { 
+	global $app; 
+	global $post; 
+	extract( $_POST ); // imports $type, $id, $num, $orderby
+	
+	switch ( $type ) { 
+		case "category": 
+			$categories = $id; 
+			$category_in = explode( ",", $categories ); 
+			
+			$args = array( 
+				'category__in' => $category_in, 
+				'numberposts' => $num, 
+				'orderby' => $orderby 
+			);
+			
+			$myposts = get_posts( $args ); 
+			$data = array(); 
+			foreach ( $myposts as $post ) { 
+				setup_postdata( $post ); 
+				$data[$post->ID] = array( "title" => get_the_title(), "imgsrc" => $app->get_image($post->ID), "link" => get_permalink() );  
+			}
+			exit(json_encode($data));  
+			break;
+		
+		case "author":  
+			$args = array( 
+				'numberposts' => $num, 
+				'author' => $id,  
+				'orderby' => 'rand' 
+			); 	
+			$myposts = get_posts( $args ); 
+			$data = array(); 
+			foreach ( $myposts as $post ) { 
+				setup_postdata( $post ); 
+				$data[$post->ID] = array( "title" => get_the_title(), "imgsrc" => $app->get_image($post->ID), "link" => get_permalink() );  
+			}
+			exit(json_encode($data)); 
+			break;
+	} 
+	
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
